58560c87853d95d0e0fa4c1128fcbef2dc2cd983
Taofeek Adegoke <mydjangopythonprojects@gmail.com>
2024-02-27 18:07:40 +0100

    added migrations

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -27,8 +27,8 @@ class User(AbstractUser):
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username', 'password']
+    # USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['username', 'password']
 
 
 class UserProfile(models.Model):
