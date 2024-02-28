a39290ce3fd654367d53e1f3d74f607ba0298bf0
Taofeek Adegoke <mydjangopythonprojects@gmail.com>
2024-02-28 07:59:09 +0100

    chore: data models refactoring

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -5,25 +5,28 @@ from django.core.exceptions import BadRequest
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from utils.utils import dummy_unique_str
+
 # from staffcalendar.models import ShiftManager
 
 
-def upload_image(instance,filename ):
-    return os.path.join('images', 'avatars', str(instance.pk), filename)
+def upload_image(instance, filename):
+    return os.path.join("images", "avatars", str(instance.pk), filename)
+
 
 def upload_xlsx(instance, filename):
     base, extension = os.path.splitext(filename)
-    if base.lower() != 'employee_batch' and extension.lower() !="xlsx":
-        return BadRequest("Invalid File-type uploaded. File name must be 'employee_batch.xlsx'")
+    if base.lower() != "employee_batch" and extension.lower() != "xlsx":
+        return BadRequest(
+            "Invalid File-type uploaded. File name must be 'employee_batch.xlsx'"
+        )
     return f"workbooks/{str(filename)}"
     # return os.path.join( 'workbooks', str(filename )
-
 
 
 # Create your models here.
 class User(AbstractUser):
     middle_name = models.CharField(max_length=50, blank=True, null=True)
-    username = models.CharField(max_length=50,unique=True)
+    username = models.CharField(max_length=50, unique=True)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
 
@@ -35,28 +38,58 @@ class UserProfile(models.Model):
     pkid = models.BigAutoField(primary_key=True, editable=False)
     id = models.UUIDField(default=uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    reader_uid = models.CharField(max_length=128,default=dummy_unique_str, unique=True)
+    reader_uid = models.CharField(max_length=128, default=dummy_unique_str, unique=True)
     profile_image = models.ImageField(upload_to=upload_image, blank=True, null=True)
-    category = models.ForeignKey("EmployeeCategory", null=True, on_delete=models.SET_NULL)
-    department = models.CharField(max_length=225)
+    category = models.ForeignKey(
+        "EmployeeCategory", null=True, on_delete=models.SET_NULL
+    )
+    # department = models.CharField(max_length=225, unique=False)
+    department = models.ForeignKey(
+        "Department", null=True, on_delete=models.SET_NULL
+    )
     location = models.ForeignKey("Location", null=True, on_delete=models.SET_NULL)
+    staff_status = models.ForeignKey("EmployeeStatus", null=True, on_delete=models.SET_NULL)
     # shift = models.ForeignKey(ShiftManager, null=True, on_delete=models.SET_NULL)
+    
 
     def __str__(self) -> str:
         return self.user.username
 
+
 class Location(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=255)
-    
+
     def __str__(self):
         return f"{self.name}"
 
-class EmployeeCategory(models.Model):
-    cat_name = models.CharField(max_length=125)
-    allowed_meal_access = models.PositiveSmallIntegerField(default=1)
+
+class Department(models.Model):
+    dept_name = models.CharField(max_length=55)
+    
+    def __str__(self):
+        return f"{self.dept_name}"
+
+class EmployeeStatus(models.Model):
+    status = models.CharField(max_length=50)
     description = models.CharField(max_length=255, blank=True, null=True)
     
+    class Meta:
+        verbose_name_plural = "Employees' status"
+        
+        
+    def __str__(self):
+        return f"{self.status}"
+
+class EmployeeCategory(models.Model):
+    cat_name = models.CharField(max_length=125)
+    meal_access = models.PositiveSmallIntegerField(default=1)
+    drink_access = models.PositiveSmallIntegerField(default=1)
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Employees' categories"
+
     def __str__(self):
         return f"{self.cat_name}"
 
@@ -66,5 +99,5 @@ class EmployeeBatchUpload(models.Model):
     date_uploaded = models.DateTimeField(auto_now_add=True)
     uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self)-> str:
+    def __str__(self) -> str:
         return self.uploaded_by.username
