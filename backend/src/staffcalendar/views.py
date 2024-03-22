78343faaabe24ec5f186da5c95e90137a8b1a7bc
Taofeek Adegoke <mydjangopythonprojects@gmail.com>
2024-03-22 03:09:50 +0100

    chore: fixed bugs on roster app

--- backend/src/staffcalendar/views.py
+++ backend/src/staffcalendar/views.py
@@ -23,55 +23,57 @@ class BatchListCreateAPIView(generics.ListCreateAPIView):
 		return BatchSerializer
 
 class RosterListCreateAPIView(generics.ListCreateAPIView):
-	queryset = MonthlyRoster.objects.all().select_related("batch")
-	serializer_class = RosterSerializer
+    queryset = MonthlyRoster.objects.all().select_related("batch")
+    serializer_class = RosterSerializer
 
-	def get_serializer_class(self):
-		if self.request.method == 'POST':
-			return RosterCreateSerializer
-		return RosterSerializer
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return RosterCreateSerializer
+        return RosterSerializer
 
-	def create(self, request, *args, **kwargs):
-		payload = user_auth(request)
-		if payload.get("auth_error", None):
-			return Response(payload, status=status.HTTP_403_FORBIDDEN)
-		
-		print("######...: ", request.data)
-		modified_request_data = self.modify_request_data(request.data)
+    def create(self, request, *args, **kwargs):
+        payload = user_auth(request)
+        if payload.get("auth_error", None):
+            return Response(payload, status=status.HTTP_403_FORBIDDEN)
 
-		# Pass the modified request data to the serializer
-		serializer = self.get_serializer(data=modified_request_data, many=True)
-		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer)
-		headers = self.get_success_headers(serializer.data)
-		return Response(serializer.data, status=201, headers=headers)
+        print("######...: ", request.data)
+        modified_request_data = self.modify_request_data(request.data)
 
+        # Pass the modified request data to the serializer
+        serializer = self.get_serializer(data=modified_request_data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=201, headers=headers)
 
-	def modify_request_data(self, data):
-		copy_data = data.copy()
-		modified_data = []
-		# Iterate over each payload item
-		for item in copy_data:
-			# Extract common information
-			start_date = item.get("start_date")
-			group = item.get("group")
-			batch = Batch.objects.filter(name__iexact=group).first().id
+    def modify_request_data(self, data):
+        copy_data = data.copy()
+        modified_data = []
+        # Iterate over each payload item
+        for item in copy_data:
+            # Extract common information
+            roster_start_date = item.get("start_date")
+            group = item.get("group")
+            batch = Batch.objects.filter(name__iexact=group).first().id
 
-			# Iterate over shifts
-			for w_day, shift_name in item['shifts'].items():
-				work_day = WorkDay.objects.filter(day_symbol=w_day).first()
-				shift = ShiftType.objects.filter(name__iexact=shift_name).first()
-				print("EXTRACTED SHIFT: ",shift)
-				shift_date, week_start_date = get_shift_date(work_day.day_symbol, start_date)
-				new_dict = {
-					"week_start_date": week_start_date,
-					"work_day": work_day.id,
-					"shift": shift.id,
-					"batch": batch,
-					"shift_date":shift_date
-				}
-				modified_data.append(new_dict)
-		return modified_data
+            # Iterate over shifts
+            for w_day, shift_name in item["shifts"].items():
+                work_day = WorkDay.objects.filter(day_symbol=w_day).first()
+                shift = ShiftType.objects.filter(name__iexact=shift_name).first()
+                print("EXTRACTED SHIFT: ", shift)
+                shift_start_date, shift_end_date, week_start_date = get_shift_date(
+                    work_day.day_symbol, shift, roster_start_date
+                )
+                new_dict = {
+                    "week_start_date": week_start_date,
+                    "work_day": work_day.id,
+                    "shift": shift.id,
+                    "batch": batch,
+                    "shift_start_date": shift_start_date,
+                    "shift_end_date": shift_end_date,
+                }
+                modified_data.append(new_dict)
+        return modified_data
 
 
 class RosterRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
